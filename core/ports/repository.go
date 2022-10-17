675944a850097c8fff53c878aab9e75d478d2373
Panupong Rong-O <tayalone@gmail.com>
2022-10-17 20:04:07 +0700

    ✨ add Business Login in Book Services

--- core/ports/repository.go
+++ core/ports/repository.go
@@ -4,5 +4,5 @@ import "github.com/tayalone/SHP-SHOW-CASE-BOOK-SRV/core/domains"
 
 /*BookRpstr is Define Behavior of "Book Repository" */
 type BookRpstr interface {
-	getByPk(uint) (domains.Book, error)
+	GetByPk(uint) (domains.Book, error)
 }
--- core/ports/service.go
+++ core/ports/service.go
@@ -4,5 +4,5 @@ import "github.com/tayalone/SHP-SHOW-CASE-BOOK-SRV/core/domains"
 
 /*BookSrv is Defining Behavior of "Book Service" */
 type BookSrv interface {
-	getByID(id uint) (domains.Book, error)
+	GetByID(id uint) (domains.Book, error)
 }
