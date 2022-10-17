d1fdff60949c6b082a352d5fc725cf41d124ff89
Panupong Rong-O <tayalone@gmail.com>
2022-10-17 21:34:10 +0700

    🚑 fix .air.toml

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/tayalone/SHP-SHOW-CASE-BOOK-SRV/repos"
 )
 
 func main() {
 	fmt.Println("Let's Make Book Services")
 
-	// repos.New()
+	repos.New()
 }
