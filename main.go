1857edfe18fe1797c08b6808f6317260f57c03e6
panupong rong-o <tayalone@gmail.com>
2022-10-18 10:04:24 +0700

    :sparkles: split rotuer from main

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/tayalone/SHP-SHOW-CASE-BOOK-SRV/core/services"
 	"github.com/tayalone/SHP-SHOW-CASE-BOOK-SRV/repos"
 	BookRepo "github.com/tayalone/SHP-SHOW-CASE-BOOK-SRV/repos/book"
+	router "github.com/tayalone/SHP-SHOW-CASE-BOOK-SRV/routers"
 )
 
 func main() {
@@ -13,37 +12,10 @@ func main() {
 	// ------- Make Repository
 	bookRepo := BookRepo.New(db)
 
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	bookSrv := services.New(bookRepo)
 
-	r.GET("/book/:id", func(c *gin.Context) {
-		/*GetIDUri is Get ID From Parmas */
-		type getIDUri struct {
-			ID uint `uri:"id" binding:"required"`
-		}
+	myRouter := router.New(bookSrv)
 
-		var gi getIDUri
-		if err := c.ShouldBindUri(&gi); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
-			return
-		}
-		b, errGetPk := bookRepo.GetByPk(gi.ID)
-		if errGetPk != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"msg": errGetPk.Error()})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": "OK",
-			"book":    b,
-		})
-
-	})
-
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	myRouter.Start()
 
 }
---
+++ routers/router.go
@@ -0,0 +1,52 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tayalone/SHP-SHOW-CASE-BOOK-SRV/core/ports"
+)
+
+/*MyRouter is Define Attr of SPI Router in Application*/
+type MyRouter struct {
+	engine *gin.Engine
+}
+
+var mr MyRouter
+
+/*New return My Router */
+func New(b ports.BookSrv) *MyRouter {
+	r := gin.Default()
+	r.GET("/book/:id", func(c *gin.Context) {
+		/*GetIDUri is Get ID From Parmas */
+		type getIDUri struct {
+			ID uint `uri:"id" binding:"required"`
+		}
+
+		var gi getIDUri
+		if err := c.ShouldBindUri(&gi); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+			return
+		}
+		b, errGetPk := b.GetByID(gi.ID)
+		if errGetPk != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": errGetPk.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "OK",
+			"book":    b,
+		})
+
+	})
+
+	mr.engine = r
+	return &mr
+}
+
+/*Start is  a trigger Router Serve API*/
+func (mr MyRouter) Start() {
+
+	mr.engine.Run(":3000")
+}
